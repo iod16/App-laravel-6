71aa2de95640d3ad35ed2cd74dfc924561eb191f
Iago David <iod16.cc@gmail.com>
2019-12-01 17:47:54 -0300

    Novas rotas e view

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/contato', function () {
+    return view('site.contact');
+});
+
+Route::post('/register', function () {
+    return '';
+});
+
+// Aceita todo tipo de requisão http
+Route::any('/any', function () {
+    return 'Any';
+});
+
+// Aceita todo tipo de requisão http
+Route::match(['get','post'],'/match', function () {
+    return 'match';
 });
